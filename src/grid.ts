a609aacab2d37f64f1b78a00642b242f9e2f5e50
Steven Almeroth <steven.almeroth@gmail.com>
2024-11-25 22:55:47 -0500

    normalize grid class

---
+++ src/grid.ts
@@ -0,0 +1,30 @@
+import { createGrid, type GridOptions } from "@ag-grid-community/core";
+
+import plans from "../data/plans.json";
+
+class SimpleGrid {
+  private readonly gridOptions: GridOptions = <GridOptions>{};
+
+  constructor() {
+    this.gridOptions = {
+      columnDefs: [{ field: "Year" }, { field: "Carrier" }, { field: "Name" }],
+      rowData: plans,
+      defaultColDef: {
+        flex: 1,
+      },
+      statusBar: {
+        statusPanels: [
+          {
+            statusPanel: "agTotalAndFilteredRowCountComponent",
+            align: "left",
+          },
+        ],
+      },
+    };
+
+    const eGridDiv: HTMLElement = <HTMLElement>document.querySelector("#app");
+    createGrid(eGridDiv, this.gridOptions);
+  }
+}
+
+export { SimpleGrid };
--- src/main.ts
+++ src/main.ts
@@ -11,33 +11,8 @@ ModuleRegistry.registerModules([ClientSideRowModelModule, StatusBarModule]);
 import { LicenseManager } from "@ag-grid-enterprise/core";
 LicenseManager.setLicenseKey("<your license key>");
 
+import { SimpleGrid } from "./grid";
+
 import "./style.css";
-
-import plans from "../data/plans.json";
-
-class SimpleGrid {
-  private readonly gridOptions: GridOptions = <GridOptions>{};
-
-  constructor() {
-    this.gridOptions = {
-      columnDefs: [{ field: "Year" }, { field: "Carrier" }, { field: "Name" }],
-      rowData: plans,
-      defaultColDef: {
-        flex: 1,
-      },
-      statusBar: {
-        statusPanels: [
-          {
-            statusPanel: "agTotalAndFilteredRowCountComponent",
-            align: "left",
-          },
-        ],
-      },
-    };
-
-    const eGridDiv: HTMLElement = <HTMLElement>document.querySelector("#app");
-    createGrid(eGridDiv, this.gridOptions);
-  }
-}
 
 new SimpleGrid();
