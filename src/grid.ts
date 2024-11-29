a39b74dd25726a9f71cfe2ed3f8d93cfd07a6b4b
Steven Almeroth <steven.almeroth@gmail.com>
2024-11-29 01:59:07 -0500

    status panel

--- src/grid.ts
+++ src/grid.ts
@@ -61,6 +61,16 @@ class SimpleGrid {
             statusPanel: "agTotalAndFilteredRowCountComponent",
             align: "left",
           },
+          { statusPanel: "agTotalRowCountComponent" },
+          { statusPanel: "agFilteredRowCountComponent" },
+          { statusPanel: "agSelectedRowCountComponent" },
+          {
+            statusPanel: "agAggregationComponent",
+            statusPanelParams: {
+              // possible values are: 'count', 'sum', 'min', 'max', 'avg'
+              aggFuncs: ["count", "sum", "min", "max", "avg"],
+            },
+          },
         ],
       },
     };
