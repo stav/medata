00226dc8c0514b0b8b299b6b42caff8961824b09
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-01 09:26:22 -0500

    normalize grid

--- src/grid.ts
+++ src/grid.ts
@@ -1,11 +1,27 @@
+// Register Modules
+
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClipboardModule } from "@ag-grid-enterprise/clipboard";
+import { RangeSelectionModule } from "@ag-grid-enterprise/range-selection";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import { StatusBarModule } from "@ag-grid-enterprise/status-bar";
+
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  StatusBarModule,
+  ClipboardModule,
+  RangeSelectionModule,
+]);
+
+// Create the Grid
+
 import { createGrid } from "@ag-grid-community/core";
 import { gridOptions } from "./options";
+import "./style.css";
 
-class SimpleGrid {
+export default class SimpleGrid {
   constructor() {
     const eGridDiv = <HTMLElement>document.querySelector("#app");
     createGrid(eGridDiv, gridOptions);
   }
 }
-
-export { SimpleGrid };
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,3 @@
-import { ModuleRegistry } from "@ag-grid-community/core";
-import { ClipboardModule } from "@ag-grid-enterprise/clipboard";
-import { RangeSelectionModule } from "@ag-grid-enterprise/range-selection";
-import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
-import { StatusBarModule } from "@ag-grid-enterprise/status-bar";
+import Grid from "./grid";
 
-ModuleRegistry.registerModules([
-  ClientSideRowModelModule,
-  StatusBarModule,
-  ClipboardModule,
-  RangeSelectionModule,
-]);
-
-import { LicenseManager } from "@ag-grid-enterprise/core";
-
-LicenseManager.setLicenseKey("<your license key>");
-
-import { SimpleGrid } from "./grid";
-
-import "./style.css";
-
-new SimpleGrid();
+new Grid();
