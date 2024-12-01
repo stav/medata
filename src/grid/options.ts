a72a778b0f94c9a36e1bd82b3d2abcf3ebd8d279
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-01 13:55:46 -0500

    cell colors

--- src/grid/options.ts
+++ src/grid/options.ts
@@ -29,14 +29,66 @@ const gridOptions: GridOptions = {
     { field: "Name", filter: true },
     { field: "Type", filter: true },
     { field: "ID", filter: true },
-    { field: "Premium", type: ["numerical", "rightAligned"] },
-    { field: "Giveback", type: ["numerical", "rightAligned"] },
-    { field: "Spc copay", type: ["numerical", "rightAligned"] },
+    {
+      field: "Premium", type: ["numerical", "rightAligned"],
+      cellClassRules: {
+        "rag-warn": "x <= -100",
+        "rag-fine": "x > -100 && x <= -10",
+        "rag-over": "x > 0",
+      },
+    },
+    {
+      field: "Giveback", type: ["numerical", "rightAligned"],
+      cellClassRules: {
+        "rag-good": "x > 1000",
+        "rag-fine": "x > 100 && x <= 1000",
+        "rag-over": "x < 0",
+      },
+    },
+    {
+      field: "Spc copay", type: ["numerical", "rightAligned"],
+      cellClassRules: {
+        "rag-good": "x === 0",
+        "rag-warn": "x < -35",
+      },
+    },
     { field: "Ambulance", type: ["numerical", "rightAligned"] },
-    { field: "MOOP", type: ["numerical", "rightAligned"] },
-    { field: "OTC", type: ["numerical", "rightAligned"] },
-    { field: "Card", type: ["numerical", "rightAligned"] },
-    { field: "Dental", type: ["numerical", "rightAligned"] },
+    { field: "ER", type: ["numerical", "rightAligned"] },
+    { field: "Urgent", type: ["numerical", "rightAligned"] },
+    {
+      field: "MOOP", type: ["numerical", "rightAligned"],
+      cellClassRules: {
+        "rag-warn": "x <= -8000",
+        "rag-fine": "x > -8000 && x <= -4000",
+        "rag-good": "x > -4000 && x < 0",
+        "rag-zero": "x === 0",
+        "rag-over": "x > 0",
+      },
+    },
+    {
+      field: "OTC", type: ["numerical", "rightAligned"],
+      cellClassRules: {
+        "rag-good": "x > 1000",
+        "rag-fine": "x >= 400 && x <= 1000",
+        "rag-over": "x < 0",
+      },
+    },
+    {
+      field: "Card", type: ["numerical", "rightAligned"],
+      cellClassRules: {
+        "rag-good": "x > 1000",
+        "rag-fine": "x >= 400 && x <= 1000",
+        "rag-over": "x < 0",
+      },
+    },
+    {
+      field: "Dental", type: ["numerical", "rightAligned"],
+      cellClassRules: {
+        "rag-good": "x >= 4000",
+        "rag-fine": "x >= 2000 && x < 4000",
+        "rag-over": "x < 0",
+      },
+    },
     { field: "Vision", type: ["numerical", "rightAligned"] },
     { field: "Hospital /day", type: ["numerical", "rightAligned"] },
     { field: "Hospital days", type: ["total", "rightAligned"] },
