85ba63ddd79f5d9677cc227d2cc02a56432e35d6
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-02 19:02:12 -0500

    cleanup

--- src/grid/options/on-filter-changed.ts
+++ src/grid/options/on-filter-changed.ts
@@ -1,16 +1,20 @@
 import type { ColDef, FilterChangedEvent } from "@ag-grid-community/core";
 
+type IModel = {
+  filter: string;
+} | null;
+
 export default function (event: FilterChangedEvent) {
-    const col = event.columns[0];
-    const model = event.api.getColumnFilterModel(col) as {
-      filter: string;
-    } | null;
-    const defs: ColDef[] = event.api.getColumnDefs() ?? [];
-    defs.forEach((def) => {
-      if (def.colId === col.getColId()) {
-        def.headerName = model?.filter;
-      }
-    });
-    event.api.setGridOption("columnDefs", defs);
-    // event.api.refreshHeader();
-  }
+  const col = event.columns[0];
+  const model = event.api.getColumnFilterModel(col) as IModel;
+  const defs: ColDef[] = event.api.getColumnDefs() ?? [];
+
+  defs.forEach((def) => {
+    if (def.colId === col.getColId()) {
+      def.headerName = model?.filter;
+    }
+  });
+
+  event.api.setGridOption("columnDefs", defs);
+  // event.api.refreshHeader();
+}
