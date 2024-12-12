ac381ba3616ae7e10381e002498d91345e0a22ce
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-12 07:23:39 -0500

    overlay

--- src/grid/options/index.ts
+++ src/grid/options/index.ts
@@ -1,4 +1,4 @@
-import type { GridOptions } from "@ag-grid-community/core";
+import type { CellKeyDownEvent, GridOptions } from "@ag-grid-community/core";
 
 import statusBar from "./status-bar";
 import columnDefs from "./column-defs";
@@ -7,6 +7,32 @@ import onRowClicked from "./on-row-clicked";
 import onFilterChanged from "./on-filter-changed";
 
 import rowData from "../../../data/plans.json";
+
+import type { INoRowsOverlayComp, INoRowsOverlayParams } from '@ag-grid-community/core';
+
+type CustomNoRowsOverlayParams = INoRowsOverlayParams & { noRowsMessageFunc: () => string };
+
+class CustomNoRowsOverlay implements INoRowsOverlayComp {
+  eGui!: HTMLElement;
+
+  init(params: CustomNoRowsOverlayParams) {
+    this.eGui = document.createElement('div');
+    this.refresh(params);
+  }
+
+  getGui() {
+    return this.eGui;
+  }
+
+  refresh(params: CustomNoRowsOverlayParams): void {
+    // console.log('refresh', params)
+    this.eGui.innerHTML = `
+            <div role="presentation" class="ag-overlay-loading-center" style="background-color: #b4bebe;">
+                <i class="far fa-frown" aria-live="polite" aria-atomic="true"> ${params.noRowsMessageFunc()} </i>
+            </div>
+        `;
+  }
+}
 
 export default <GridOptions>{
   rowData,
@@ -26,6 +52,20 @@ export default <GridOptions>{
     const active = key === 'a' && keyDown.ctrlKey === false && keyDown.altKey === false;
     if (active) {
       console.log('Key Down:', key, active, event.data);
+      const api = event.api;
+      const html = `
+        <div role="presentation" class="ag-overlay-loading-center" style="background-color: #b4bebe;">
+            <i class="far fa-frown" aria-live="polite" aria-atomic="true"> ${event.data} </i>
+        </div>
+      `;
+      const element = document.createElement('div')
+      element.innerHTML = html
+      api.showNoRowsOverlay();
     }
+  },
+  noRowsOverlayComponent: CustomNoRowsOverlay,
+  noRowsOverlayComponentParams: {
+    noRowsMessageFunc: () =>
+      "No rows found at: " + new Date().toLocaleTimeString(),
   },
 };
