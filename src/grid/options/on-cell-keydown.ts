0ab7cb4ea4ea5243af988ad4fe50b9e79590dab4
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-12 21:04:01 -0500

    opened max of 1

--- src/grid/options/on-cell-keydown.ts
+++ src/grid/options/on-cell-keydown.ts
@@ -1,14 +1,40 @@
 import type { CellKeyDownEvent } from "@ag-grid-community/core";
 
-import dimbox from 'dimbox';
+import dimbox from "dimbox";
+
+dimbox.setConfig({
+  onAfterOpen: function () {
+    opened++;
+    if (opened !== 1) {
+      console.error(
+        `Opened Error: opened count is "${opened}" but it should be 1`
+      );
+    }
+  },
+  onBeforeClose: function () {
+    opened--;
+    if (opened !== 0) {
+      console.error(
+        `Closing Error: opened count is "${opened}" but it should be 1`
+      );
+    }
+  },
+});
+
+let opened = 0;
 
 export default function (event: CellKeyDownEvent) {
   const keyDown = <KeyboardEvent>event.event;
   const key = keyDown.key;
-  const active = key === 'a' && keyDown.ctrlKey === false && keyDown.altKey === false;
+  const active =
+    key === "a" && keyDown.ctrlKey === false && keyDown.altKey === false;
   if (active) {
-    const a = document.getElementById('modal-anchor');
-    const box = document.getElementById('modal-box');
+    if (opened > 0) {
+      dimbox.close();
+      return;
+    }
+    const a = document.getElementById("modal-anchor");
+    const box = document.getElementById("modal-box");
     if (box) {
       box.innerHTML = JSON.stringify(event.data);
       dimbox.open(a);
