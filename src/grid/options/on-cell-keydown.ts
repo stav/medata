848cc30e1c2ad7a59e727493b6aac4e33660464a
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-13 21:54:43 -0500

    alerted

--- src/grid/options/on-cell-keydown.ts
+++ src/grid/options/on-cell-keydown.ts
@@ -1,10 +1,20 @@
 import type { CellKeyDownEvent } from "@ag-grid-community/core";
 
+import Swal from "sweetalert2";
+
 export default function (event: CellKeyDownEvent) {
   const keyDown = <KeyboardEvent>event.event;
   const key = keyDown.key;
   const active = key === 'a' && keyDown.ctrlKey === false && keyDown.altKey === false;
   if (active) {
-    console.log('Key Down:', key, active, event.data);
+    console.log("Key Down:", key, active, event.data, Swal);
+    const json = JSON.stringify(event.data);
+    Swal.fire({
+      title: "Info.",
+      // text: 'Do you want to continue',
+      html: `'<code>${json}</code>'`,
+      icon: "info",
+      confirmButtonText: "Cool",
+    });
   }
 }
