a72ed39a4e742398cd9ecb02b52c5fd8ca701884
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-13 21:47:10 -0500

    login
    esc: doesn't work
    pro: allows multiples
    con: allows multiples

--- src/grid/options/on-cell-keydown.ts
+++ src/grid/options/on-cell-keydown.ts
@@ -1,10 +1,45 @@
 import type { CellKeyDownEvent } from "@ag-grid-community/core";
 
+import { silverBox } from "../../../public/silverBox";
+
 export default function (event: CellKeyDownEvent) {
   const keyDown = <KeyboardEvent>event.event;
   const key = keyDown.key;
   const active = key === 'a' && keyDown.ctrlKey === false && keyDown.altKey === false;
   if (active) {
-    console.log('Key Down:', key, active, event.data);
+    // @ts-check
+    console.log('Key Down:', key, active, event.data, silverBox);
+
+    silverBox({
+      customIcon: "/public/src/images/loginExample.png",
+      title: {
+             text: "Login"
+      },
+      centerContent: true,
+      text: "Enter your account information",
+      footer: "<a href='#'>Forgot your password?</a>",
+      showCloseButton: true,
+      confirmButton: {
+             text: "Login",
+             closeOnClick: false
+      },
+      theme: 'dark',
+      cancelButton: {},
+      input: [
+             {
+                    label: "Username",
+                    type: "text",
+                    placeHolder: "Enter your username",
+                    maxLength: 30
+             },
+             {
+                    label: "Password",
+                    type: "password",
+                    placeHolder: "Enter your password",
+                    hint: "Pick a strong password."
+             }
+      ]
+})
+
   }
 }
