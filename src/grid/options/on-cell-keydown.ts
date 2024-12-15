4967359525dd53e734a5eff831db69329bfb3366
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-15 14:18:14 -0500

    normalize active

--- src/grid/options/on-cell-keydown.ts
+++ src/grid/options/on-cell-keydown.ts
@@ -2,18 +2,19 @@ import type { CellKeyDownEvent } from "@ag-grid-community/core";
 
 import Swal from "sweetalert2";
 
+function active(keyDown: KeyboardEvent): boolean {
+  return (
+    keyDown.key === "a" && keyDown.ctrlKey === false && keyDown.altKey === false
+  );
+}
+
 export default async function (event: CellKeyDownEvent) {
-  const keyDown = <KeyboardEvent>event.event;
-  const key = keyDown.key;
-  const active =
-    key === "a" && keyDown.ctrlKey === false && keyDown.altKey === false;
-  if (active) {
-    console.log("Key Down:", key, active, event.data, Swal);
+  if (active(event.event as KeyboardEvent)) {
+    console.log("Key Down on row", event.data);
     const json = JSON.stringify(event.data);
     const text = await gogetit();
     Swal.fire({
       title: "Info.",
-      // text: 'Do you want to continue',
       html: `'<p><code>${json}</code></p><pre>${text}</pre>'`,
       icon: "info",
       confirmButtonText: "Cool",
