0fe838e9c22614399f2ef507cf1c9ff3c02c83d2
Steven Almeroth <steven.almeroth@gmail.com>
2024-12-15 20:33:07 -0500

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, type ServerOptions, type UserConfig } from 'vite'
+import { defineConfig, type ServerOptions, type UserConfig } from "vite";
 
-import proxy from './src/http/proxy'
+import proxy from "./src/http/proxy";
 
-const server: ServerOptions = { proxy }
+const server: ServerOptions = { proxy };
 
-const config: UserConfig = { server }
+const config: UserConfig = { server };
 
-export default defineConfig(config)
+export default defineConfig(config);
